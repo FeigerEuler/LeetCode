cb5626c71133943c7be0b0d15dfd1b2afb02af73
mhf <804941144@qq.com>
2022-01-04 19:48:49 +0800

    init

---
+++ src/main/java/com/mhh/leetcode/solutions/Solution.java
@@ -0,0 +1,13 @@
+package com.mhh.leetcode.solutions;
+
+
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+@Repository
+public class Solution {
+
+    public void hello(){
+        System.out.println("hello world!");
+    }
+}
---
+++ src/test/java/com/mhh/leetcode/solution/SolutionsTest.java
@@ -0,0 +1,27 @@
+package com.mhh.leetcode.solution;
+
+
+import com.mhh.leetcode.LeetcodeApplication;
+import com.mhh.leetcode.solutions.Solution;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = LeetcodeApplication.class)
+public class SolutionsTest {
+
+    @Autowired
+    private Solution solution;
+
+    @Test
+    public void helloTest(){
+        solution.hello();
+    }
+
+}
